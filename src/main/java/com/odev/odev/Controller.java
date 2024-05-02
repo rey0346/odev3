844bd97cb4b2534ed362c200e8f2423ed88a2ba0
rey0346 <ramzanenes1516@gmail.com>
2024-05-02 17:11:09 +0300

    sd

--- src/main/java/com/odev/odev/Controller.java
+++ src/main/java/com/odev/odev/Controller.java
@@ -1,10 +1,11 @@
 package com.odev.odev;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/controller")
-
+@RestController()
+@RequestMapping("/controller")
 public class Controller {
     @GetMapping("/all")
     public String getProjeBilgileri(){
